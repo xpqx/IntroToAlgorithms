1e59ea1e5ada1fcbb0f8b09b04880f0ac08ca35c
csula cstutor <74371276+xpqx@users.noreply.github.com>
2021-01-05 15:30:56 -0800

    upload quicksort

---
+++ Ch7/schaums-c++-quick-sort/quickSort1.cpp
@@ -0,0 +1,87 @@
+//quickSort1.cpp
+//demonstrates simple versionof quick sort
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class ArrayIns{
+private:
+	vector<double>(theVect);
+	int nElems;
+public:
+}
+
+ArrayIns(int max):nElems(0){
+	theVect.resize(max);
+}
+
+void insert(double value){
+	theVect[nElems] = value;
+	nElems++;
+}
+
+void display(){
+	cout << "A=";
+	for (int j =0; j < nElems; j++){
+		cout << theVect[j] << " ";
+	cout << endl;
+	}
+
+void quickSort(){
+	recQuickSort(0, nElems - 1);
+}
+
+recQuickSort(int left, int right){
+	if(right - left <= 0)
+		return;
+	else {
+		double pivot = theVect[right];
+
+		int partition = partitionIt(left, right, pivot);
+		recQuickSort(left, partition - 1);
+		recQuickSort(partition + 1, right);
+	}
+}//end recQuickSort()
+
+int partition(int left, int right, double pivot){
+	int leftMark = left - 1;
+	int rightMark = right;
+	while (true){
+		while (theVect[++leftMark] < pivot)
+			;
+		while(rightMark > 0 && theVect[--rightMark] > pivot)
+			;
+		if (leftMark >= rightMark)
+			break;
+		else
+			swap(leftMark, rightMark);
+	}
+	swap(leftMark, right);
+	return leftMark;
+}
+
+void swap(int dex1, int dex2){
+	double temp = theVect[dex1];
+	theVect[dex1] = theVect[dex2];
+	theVect[dex2] = temp;
+	}
+};
+
+int main(){
+	time_t aTime;
+	int maxSize = 16;
+	ArrayIns arr(maxSize);
+	srand(static_cast<unsigned>(time(&aTime)) );
+
+	for (int j = 0; j< maxSize; j++){
+		double n = rand()%99;
+		arr.insert(n);
+	}
+	arr.display();
+	arr.quickSort();
+	arr.display();
+	return 0;
+}
